77a17396587a6847e2248687babca59ca576f0ff
Emmanuel Diaz <emmanuel050216@gmail.com>
2026-03-23 13:26:12 +0100

    Fix sync: incluir nominas editadas en Firebase, eliminar SW cache, anti-cache headers

--- sw.js
+++ sw.js
@@ -1,31 +1,13 @@
-const CACHE_NAME = 'diaz-gonzalez-v9';
-const URLS_TO_CACHE = [
-  './',
-  './index.html',
-  './manifest.json',
-  './icon-192.png',
-  './icon-512.png',
-  'https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@400;500;600;700&family=JetBrains+Mono:wght@400;600&display=swap'
-];
-
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(URLS_TO_CACHE)));
-  self.skipWaiting();
-});
-
+// Service Worker v10 - Auto-destructor
+// Se desregistra a sí mismo y limpia todos los caches
+self.addEventListener('install', () => self.skipWaiting());
 self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(keys =>
-    Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
-  ));
-  self.clients.claim();
-});
-
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    fetch(e.request).then(response => {
-      const clone = response.clone();
-      caches.open(CACHE_NAME).then(cache => cache.put(e.request, clone));
-      return response;
-    }).catch(() => caches.match(e.request).then(r => r || caches.match('./index.html')))
+  e.waitUntil(
+    Promise.all([
+      caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k)))),
+      self.clients.claim(),
+      self.registration.unregister()
+    ])
   );
 });
+self.addEventListener('fetch', e => e.respondWith(fetch(e.request)));
